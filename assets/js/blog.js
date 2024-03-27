fa8183e6d9fd0fb69828568398ef10ccb7b24dcc
Soko77788 <NickSokolowski7@gmail.com>
2024-03-27 16:01:01 -0500

    Fixed blog appended and overlapping footer

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -7,12 +7,16 @@ for (const post of blogPosts) {
     //div to hold blog post
     const h2 = document.createElement("h2")
     //create h2 to hold title and description
-    h2.innerText = post.title
+    h2.innerText = `Title: ${post.title}`
     //create p 
     const p = document.createElement("p")
-    p.innerText = post.content
+    p.innerText = `Blog Post: ${post.content}`
+    //create 2nd p 
+    const p2 = document.createElement("p")
+    p2.innerText = `Author: ${post.username}`
     div.appendChild(h2)
     div.appendChild(p)
+    div.appendChild(p2)
 
     submissionsDiv.appendChild(div)
     // append to submissions div
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,3 +1,4 @@
+
 document.getElementById("form").addEventListener("submit", function(event) {
     event.preventDefault(); 
 
@@ -18,6 +19,7 @@ const blogPosts = JSON.parse(localStorage.getItem("blogPosts")) || []
 localStorage.setItem("blogPosts", JSON.stringify(blogPosts));
 
 
+
     window.location.href = "file:///Users/sokos6/Desktop/my-blog-hw4/blog.html";
 
     document.getElementById("form").reset();
@@ -27,65 +29,4 @@ localStorage.setItem("blogPosts", JSON.stringify(blogPosts));
 
 
 
-// let submissionElement = 
-// document.createElement("div");
-// submissionElement.innerHTML = 
-// "Title:" + formData.title + 
-// "Content:" + formData.content + 
-// "Username:" + formData.username;
 
-// let submissionsContainer = 
-// document.getElementById("submissions");
-
-// submissionsContainer.appendChild(submissionElement);
-
-// const collectFormData = function() {
-
-
-// let formDataArray = [];
-
-// while(true) {
-//     let username =
-//     document.getElementById("username").value;
-
-//         if (username === null) {
-//             break;
-//         }
-
-//     let title = 
-//     document.getElementById("title").value;
-
-//         if (title === null) {
-//             break;
-//         }
-
-//     let content =
-//     document.getElementById("content").value;
-
-//         if(content === null) {
-//             break;
-//         }
-//         else {
-//             break;
-//         }
-
-//     let formData = {
-//         username: username,
-//         title: title,
-//         content: content
-//     };
-
-//     formDataArray.push(formData);
-//     console.log(formDataArray)
-
-// };
-
-// return formDataArray
-// }
-
-// localStorage.setItem("formDataArray", JSON.stringify(formDataArray));
-
-
-//     window.location.href = "file:///Users/sokos6/Desktop/my-blog-hw4/blog.html";
-
-//     document.getElementById("form").reset();
